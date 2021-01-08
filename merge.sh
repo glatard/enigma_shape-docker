0da438b0d25f931f88e3341cba68c7cca35990fc
Tristan Glatard <tristan.glatard@concordia.ca>
2021-01-08 17:57:21 -0500

    Tested merge script

--- merge.sh
+++ merge.sh
@@ -1,11 +1,14 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 ###
 # Usage: merge.sh <dir1> <dir2> <dir3> ...
 ###
 
+set -e
 
 study="study"
+
+echo Build text files
 
 for b in 10 11 12 13 17 18 26 49 50 51 52 53 54 58
 do
@@ -15,12 +18,21 @@ do
         \rm -f $out
         for s in $*
         do
-            od -f shape/${s}/${m}_${b}.raw | gawk 'BEGIN{a="'$s'"}{for(i=2;i<NF+1;i++)a=a" "$i}END{print a}' >> $out
+            od -f ${s}/*/${m}_${b}.raw | gawk 'BEGIN{a="'$s'"}{for(i=2;i<NF+1;i++)a=a" "$i}END{print a}' >> $out
         done
     done
 done
 
-\rm merge.R
+echo Add header and remove NAs
+
+for f in FS6.0.${study}.*.txt
+do 
+	gawk 'BEGIN{f="'$f'";gsub(".txt",".clean.txt.gz",f)}{if(a<NF)a=NF;b[$1]=$0;c[$1]=NF}END{for(i=0;i<a;i++)if(i==0){h="bigrfullname"}else h=h" Vert_"i;print h | "gzip -9c > "f; for (d in b) if (c[d]==a) print b[d] | "gzip -9c > "f}' $f
+done
+
+echo Build and run R script
+
+\rm -f merge.R
 cat << EOF > merge.R
 
 gzfiles=system("ls FS6.0.${study}.*.clean.txt.gz",intern=T)
