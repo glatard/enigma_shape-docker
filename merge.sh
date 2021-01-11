a5d2b442d17e4d1346d01404adb395fdba9ea1c3
Tristan Glatard <tristan.glatard@concordia.ca>
2021-01-11 08:39:59 -0500

    Shell checked

--- merge.sh
+++ merge.sh
@@ -10,32 +10,32 @@ study="study"
 
 echo Build text files
 
+prefix="FS6.0.${study}"
 for b in 10 11 12 13 17 18 26 49 50 51 52 53 54 58
 do
     for m in "thick" "LogJacs"
     do
-        out="FS6.0.${study}."${m}"."${b}".txt"
+        out="${prefix}.${m}.${b}.txt"
         \rm -f $out
-        for s in $*
+        for s in "$@"
         do
-            od -f ${s}/*/${m}_${b}.raw | gawk 'BEGIN{a="'$s'"}{for(i=2;i<NF+1;i++)a=a" "$i}END{print a}' >> $out
+            od -f "${s}"/*/${m}_${b}.raw | gawk 'BEGIN{a="'$s'"}{for(i=2;i<NF+1;i++)a=a" "$i}END{print a}' >> $out
         done
     done
 done
 
 echo Add header and remove NAs
 
-for f in FS6.0.${study}.*.txt
-do 
-	gawk 'BEGIN{f="'$f'";gsub(".txt",".clean.txt.gz",f)}{if(a<NF)a=NF;b[$1]=$0;c[$1]=NF}END{for(i=0;i<a;i++)if(i==0){h="bigrfullname"}else h=h" Vert_"i;print h | "gzip -9c > "f; for (d in b) if (c[d]==a) print b[d] | "gzip -9c > "f}' $f
+for f in "${prefix}".*.txt
+do
+    gawk 'BEGIN{f="'$f'";gsub(".txt",".clean.txt.gz",f)}{if(a<NF)a=NF;b[$1]=$0;c[$1]=NF}END{for(i=0;i<a;i++)if(i==0){h="bigrfullname"}else h=h" Vert_"i;print h | "gzip -9c > "f; for (d in b) if (c[d]==a) print b[d] | "gzip -9c > "f}' "$f"
 done
 
 echo Build and run R script
 
-\rm -f merge.R
 cat << EOF > merge.R
 
-gzfiles=system("ls FS6.0.${study}.*.clean.txt.gz",intern=T)
+gzfiles=system("ls ${prefix}.*.clean.txt.gz",intern=T)
 
 for (f in gzfiles){
     fX=gsub(".gz","",f)
@@ -50,3 +50,7 @@ for (f in gzfiles){
 EOF
 
 Rscript merge.R
+
+echo cleanup
+\rm -f ${prefix}* 
+\rm -f merge.R
